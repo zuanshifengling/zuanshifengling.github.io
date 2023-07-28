957f2e3459369fc624f7cd6e4401ff180ac7f7cb
zuanshifengling <zuanshifengling@163.com>
2023-07-28 19:26:02 +0800

    Site updated: 2023-07-28 19:25:46

--- prvi/js/cu.js
+++ prvi/js/cu.js
@@ -123,3 +123,45 @@ function createtime() {
 setInterval("createtime()",250);
 
 
+
+
+
+const cards = document.querySelectorAll('.index-card')
+if (cards.length) {
+  document.querySelector('.row').setAttribute('style', 'overflow: hidden;')
+  const coefficient = document.documentElement.clientWidth > 768 ? .5 : .3
+  const origin = document.documentElement.clientHeight - cards[0].getBoundingClientRect().height * coefficient
+
+  function throttle(fn, wait) {
+    let timer = null;
+    return function () {
+      const context = this;
+      const args = arguments;
+      if (!timer) {
+        timer = setTimeout(function () {
+          fn.apply(context, args);
+          timer = null;
+        }, wait)
+      }
+    }
+  }
+
+  function handle() {
+    cards.forEach(card => {
+      card.setAttribute('style', `--state: ${(card.getBoundingClientRect().top - origin) < 0 ? 1 : 0};`)
+    })
+    console.log(1)
+  }
+
+
+  document.addEventListener("scroll", throttle(handle, 100));
+}
+
+// // 加载动画
+// // 获取动画
+// let loader = document.querySelector(".spinner");
+// // 加载完成事件
+// window.onload = function () {
+//     // 加载完成，隐藏动画，显示内容
+//     loader.style.display = "none";
+// }
